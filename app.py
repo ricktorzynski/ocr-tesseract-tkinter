d02b47f349021e983546314620c565cae2720bbf
ricktorzynski <ricktorzynski@gmail.com>
2019-01-14 17:08:37 -0500

    Modifying app

--- app.py
+++ app.py
@@ -1,35 +1,44 @@
 import tkinter as tk
-import string
-from PIL import ImageTk, Image
-from tkinter import filedialog, messagebox
-import time, os, random
-import cv2
+from tkinter import ttk, filedialog, messagebox
+import time, os, random, string, argparse
 from pathlib import Path
 import numpy as np
+from PIL import ImageTk, Image
+import cv2
 import pytesseract as tess
-import argparse
+
 
 class Page(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("OCR Using Tesseract")
-        self.shared_data = {
-            "fileName": tk.StringVar()
-        }
+        
         container=tk.Frame(self)
         container.grid()
-        print(cv2.__version__)
-        # Title
+
+        # menu
+        self.option_add('*tearOff', False)
+        menubar = tk.Menu(self)
+        self.config(menu = menubar)
+        file = tk.Menu(menubar)
+        help_ = tk.Menu(menubar)
+
+        menubar.add_cascade(menu = file, label = "File")
+        file.add_command(label = 'Open...', command=lambda:self.show_image())
+        menubar.add_cascade(menu = help_, label = "Help")
+        help_.add_command(label = 'About', command=lambda:self.about())
+
+        # title
         self.empty_name=tk.Label(self, text="OCR Using Tesseract - Version 0.5", font=("Arial", 16))
         self.empty_name.grid(row=0, column=0, pady=5, padx=10, sticky="sw")
 
-        # Intro
+        # intro
         self.intro_lbl = tk.Label(self, text="Demonstration of using Python and Pytesseract in a desktop application for OCR.",
                                   font=("Arial", 11), fg="#202020")
         self.intro_lbl.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="nw")
 
-        # Select image                          
+        # select image                          
         self.browse_lbl = tk.Label(self, text="Select Image :", font=("Arial", 10), fg="#202020")
         self.browse_lbl.grid(row=4, column=0, columnspan=3, padx=24, pady=10, sticky="w")
 
@@ -40,81 +49,82 @@ class Page(tk.Tk):
                                     command=lambda:self.show_image())
         self.browse_btn.grid(row=4, column=0, padx=310, pady=10, columnspan=3, sticky="w")
         
-        # File selection
-
+        # file info
         self.lbl_filename = tk.Label(self, text="File Name: ", font=("Arial", 10), fg="#202020")
-        self.lbl_filename.grid(row=5, column=0, pady=0, padx=10, columnspan=3, sticky="nw")
-
-        self.lbl_filesize = tk.Label(self, text="File Size: ", font=("Arial", 10), fg="#202020")
-        self.lbl_filesize.grid(row=6, column=0, pady=0, padx=10, sticky="nw")
-
-        self.text_file_size=tk.StringVar()
-        self.lbl_filesize_01 = tk.Label(self, textvariable=self.text_file_size, font=("Arial", 10), fg="#202020")
-        self.lbl_filesize_01.grid(row=6, column=0, pady=0, padx=75, columnspan=3, sticky="nw")
+        self.lbl_filesize = tk.Label(self, text="File Size: ", font=("Arial", 10), fg="#202020") 
 
         self.label_text_x = tk.StringVar()
         self.lbl_filename_01 = tk.Label(self, textvariable=self.label_text_x, font=("Arial", 10),fg="#202020")
-        self.lbl_filename_01.grid(row=5, column=0, pady=0, padx=85, columnspan=3, sticky="nw")
-
+        
+        self.text_file_size=tk.StringVar()
+        self.lbl_filesize_01 = tk.Label(self, textvariable=self.text_file_size, font=("Arial", 10), fg="#202020")
+        
         # place holder for document thumbnail
         self.lbl_image = tk.Label(self, image="")
         self.lbl_image.grid(row=8, column=0, pady=25, padx=10, columnspan=3, sticky="nw")
+
+        # status text
+        self.label_text_progress = tk.StringVar()
+        self.scan_progress = tk.Label(self, textvariable=self.label_text_progress, font=("Arial", 10),fg="#0000ff")
         
         # scan button
-        self.scan_btn = tk.Button(self, text="      Scan      ", bg="#ffffff", relief="flat",
-                                  width=10, command=lambda:self.ocr())
-
-        # save ocr text button
-        self.save_btn = tk.Button(self, text="      Save      ", bg="#ffffff", relief="flat",
-                                  width=10, command="")
+        self.scan_btn = tk.Button(self, text="     Process     ", bg="#ffffff", relief="flat",
+                                 width=10, command=lambda:self.ocr())
+        # clear ocr text button
+        self.clear_btn = tk.Button(self, text="     Clear      ", bg="#ffffff", relief="flat",
+                                  width=10, command=lambda:self.clearOcr())
         # text area to place text
-        self.ocr_text = tk.Text(self, height=25, width=38)
-        
-        
-        
-        # spacer
-        self.empty_01 = tk.Label(self, text="")
-        self.empty_01.grid(row=29, column=0, columnspan=3, sticky="nsew", pady=152)
-
+        self.ocr_text = tk.Text(self, height=25, width=38)  
+       
     
     def show_image(self):
         global path
         
         # open file dialog
-        self.path = filedialog.askopenfilename(defaultextension="*.png", filetypes = (("PNG","*.png"),("JPG", "*.jpg")))
+        self.path = filedialog.askopenfilename(defaultextension="*.jpg", filetypes = (("JPG", "*.jpg"),("PNG","*.png")))
         self.browse_entry.delete(0, tk.END)
         self.browse_entry.insert(0, self.path)
+        
+        self.label_text_progress.set("Image loaded - ready to be processed.");
+        self.scan_progress.grid(row=18, column=0, padx=10, pady=0,
+                           columnspan=3, sticky="w")
 
+        # resize image
         cv_img = cv2.cvtColor(cv2.imread(self.path), cv2.COLOR_BGR2RGB)
         height, width, no_channels = cv_img.shape
 
-        # resize image
-        HEIGHT = 400
-      
+        HEIGHT = 400   
         imgScale = HEIGHT/height
         newX, newY = cv_img.shape[1]*imgScale, cv_img.shape[0]*imgScale
         newimg = cv2.resize(cv_img, (int(newX), int(newY)))
         photo = ImageTk.PhotoImage(image = Image.fromarray(newimg))
         
-        
-        
-        #photo=ImageTk.PhotoImage(file=self.path)
+        # show image
         self.lbl_image.configure(image=photo)
         self.lbl_image.image=photo
 
-        scan_btn_mid = int(newX/2) - 30;
-        self.scan_btn.grid(row=18, column=0, padx=scan_btn_mid, pady=0, columnspan=3, sticky="w")
+        # show file information
+        self.lbl_filename.grid(row=5, column=0, pady=0, padx=10, columnspan=3, sticky="nw")
+        self.lbl_filename_01.grid(row=5, column=0, pady=0, padx=85, columnspan=3, sticky="nw")
+        self.lbl_filesize.grid(row=6, column=0, pady=0, padx=10, sticky="nw")
+        self.lbl_filesize_01.grid(row=6, column=0, pady=0, padx=75, columnspan=3, sticky="nw")
 
+        # add scan button
+        scan_btn_mid = int(newX/2) - 40;       
+        self.scan_btn.grid(row=17, column=0, padx=scan_btn_mid, pady=10,
+                           columnspan=3, sticky="w")
         self.ocr_text.grid(row=8, column=0, padx=350, pady=26, columnspan=3, sticky="w")
         
         # set the filename
         self.label_text_x.set(os.path.basename(self.path))
+        
         # set the filesize
         self.text_file_size.set(os.path.getsize(self.path))
         
 
     def ocr(self):
-
+        self.label_text_progress.set("Image processing complete.");
+        
         # load the example image and convert it to grayscale
         self.ocr_image = cv2.imread(self.path)
         gray = cv2.cvtColor(self.ocr_image, cv2.COLOR_BGR2GRAY)
@@ -129,33 +139,37 @@ class Page(tk.Tk):
         ofilename = os.path.join('./scans',"{}.png".format(os.getpid()))
         cv2.imwrite(ofilename, gray)
 
-
+        # create a thumbnail of processed image
         corrected_img = cv2.cvtColor(cv2.imread(ofilename), cv2.COLOR_BGR2RGB)
         cheight, cwidth, cno_channels = corrected_img.shape
 
         # resize image
         HEIGHT = 400
-        
         cimgScale = HEIGHT/cheight
         cnewX, cnewY = corrected_img.shape[1]*cimgScale, corrected_img.shape[0]*cimgScale
         cnewimg = cv2.resize(corrected_img, (int(cnewX), int(cnewY)))
         cphoto = ImageTk.PhotoImage(image = Image.fromarray(cnewimg))
-        
-        #photo=ImageTk.PhotoImage(file=self.path)
+
+        # replace the unprocessed thumbnail
         self.lbl_image.configure(image=cphoto)
         self.lbl_image.image=cphoto
-      
+
         # perform OCR on the processed image
-        text = tess.image_to_string(Image.open(ofilename))
-      
+        ocrtext = tess.image_to_string(Image.open(ofilename))
+
+        # add text to end of textbox
+        self.ocr_text.insert(tk.END,ocrtext)
+        self.clear_btn.grid(row=17, column=0, padx=440, pady=10,
+                           columnspan=3, sticky="w")
+
+    def clearOcr(self):
+        # clear the textbox
+        self.ocr_text.delete(1.0, tk.END)
+
+    def about(self):
+        # show about message
+        messagebox.showinfo(title = 'About', message = 'This is a demo Tkinter project by Rick Torzynski')
         
-
-
-
-        
-
-
-       
 
 if __name__ == "__main__":
     app = Page()
